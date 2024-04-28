aadc1daab0a2cc069f86cb5971e25985a5bbe22a
solodovnykov <solodovnikov.toni@gmail.com>
2024-04-28 17:21:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 app.post('/notification', upload.none(), async (req, res) => {
     try {
-        console.log("IP: ", req.headers['HTTP_X_REAL_IP']);
+        console.log("IP: ", req.headers['x-real-ip'] || req.connection.remoteAddress);
         const payment = await Payment.create(req.body);
         res.status(200).json(payment);
     } catch (error) {
